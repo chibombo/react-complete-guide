77ff269035c538b478ba77e44c1628d099e476b8
chibombo <genaro.arvizu95@gmail.com>
2021-07-28 23:07:41 -0500

    Module-3: React basics and components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Expenses from './components/Expenses/Expenses';
 function App() {
+
+  const expenses = [
+    { id: 'a1', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 2, 28) },
+    { id: 'a2', title: 'Xbox Pass', amount: 15.00, date: new Date(2021, 7, 28) },
+    { id: 'a3', title: 'Zelda Game', amount: 60.00, date: new Date(2021, 7, 15) },
+    { id: 'a4', title: 'iPad', amount: 449.99, date: new Date(2021, 2, 29) }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+        <Expenses items = {expenses}></Expenses>
     </div>
   );
 }
