be852c070b91e78b207cb7c5efbcde951435ad79
chibombo <genaro.arvizu95@gmail.com>
2021-07-30 18:12:55 -0500

    Module-4: React State & Working with Events

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+
+function NewExpense(props) {
+
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        props.onAddExpense(expenseData);
+        console.log(expenseData);
+    };
+
+    return (
+        <div className = "new-expense">
+            <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler}></ExpenseForm>
+        </div>
+    );
+}
+
+export default NewExpense;
