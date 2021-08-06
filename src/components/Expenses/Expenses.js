d179e773f1e07c7e1cbb2bf4b249b37d9e6ee951
chibombo <genaro.arvizu95@gmail.com>
2021-08-05 23:23:26 -0500

    Module-5 Chart Added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseFilter from './ExpenseFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 import './ExpenseFilter.css';
 
@@ -19,7 +20,8 @@ function Expenses(props) {
     return (
         <div>
             <Card className="expenses">
-                <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />                
+                <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                <ExpensesChart expenses = {filteredExpenses}/>                
                 <ExpensesList items = {filteredExpenses}/>
             </Card>
         </div>
